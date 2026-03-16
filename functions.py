661e974f73cfd2c3ed51dd10f60dca498081e457
Federico Mejia <federicomejbe@unisabana.edu.co>
2026-03-16 16:10:50 -0500

    trail 9

--- functions.py
+++ functions.py
@@ -1,7 +1,5 @@
 def promedio_estudiantes(calificaciones):
     if len(calificaciones) == 0:
-        return 0
+        return 0.0
     promedio = sum(calificaciones) / len(calificaciones)
     return float(promedio)
-calificaciones = [85, 92 ,78]
-print(promedio_estudiantes(calificaciones))
