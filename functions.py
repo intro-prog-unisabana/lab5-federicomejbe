2616e52cca421c52874d59cc12f9958c7f7beaf6
Federico Mejia <federicomejbe@unisabana.edu.co>
2026-03-17 09:45:34 -0500

    trail 11

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-def promedio_estudiantes(calificaciones):
+def promedio_estudiante(calificaciones):
     if len(calificaciones) == 0:
         return 0.0
     promedio = sum(calificaciones) / len(calificaciones)
--- multiple_files.py
+++ multiple_files.py
@@ -1,8 +1,8 @@
-import utils
+from utils import *
 mensaje = input("Please type your message\n")
 
-mensaje_flip = utils.flip(mensaje)
-mensaje_a = utils.count_letters(mensaje, "a")
+mensaje_flip = flip(mensaje)
+mensaje_a = count_letters(mensaje, "a")
 mensaje_codificado = mensaje_flip + str(mensaje_a)
 
 print(f"Your encoded message is: {mensaje_codificado}")
